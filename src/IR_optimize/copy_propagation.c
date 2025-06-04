d2d54b3748d1e422ba161135ec233d3936ca07a9
juven <221220019@smail.nju.edu.cn>
2025-06-04 17:27:37 +0800

    finish lab5

--- src/IR_optimize/copy_propagation.c
+++ src/IR_optimize/copy_propagation.c
@@ -4,21 +4,23 @@
 
 #include <copy_propagation.h>
 
-
-void Fact_def_use_init(Fact_def_use *fact, bool is_top) {
+void Fact_def_use_init(Fact_def_use *fact, bool is_top)
+{
     fact->is_top = is_top;
     Map_IR_var_IR_var_init(&fact->def_to_use);
     Map_IR_var_IR_var_init(&fact->use_to_def);
 }
 
-void Fact_def_use_teardown(Fact_def_use *fact) {
+void Fact_def_use_teardown(Fact_def_use *fact)
+{
     Map_IR_var_IR_var_teardown(&fact->def_to_use);
     Map_IR_var_IR_var_teardown(&fact->use_to_def);
 }
 
 //// ============================ Dataflow Analysis ============================
 
-static void CopyPropagation_teardown(CopyPropagation *t) {
+static void CopyPropagation_teardown(CopyPropagation *t)
+{
     for_map(IR_block_ptr, Fact_def_use_ptr, i, t->mapInFact)
         RDELETE(Fact_def_use, i->val);
     for_map(IR_block_ptr, Fact_def_use_ptr, i, t->mapOutFact)
@@ -28,55 +30,69 @@ static void CopyPropagation_teardown(CopyPropagation *t) {
 }
 
 static bool
-CopyPropagation_isForward (CopyPropagation *t) {
+CopyPropagation_isForward(CopyPropagation *t)
+{
     // TODO: isForward?
-    TODO();
+    // TODO();
+    return true;
 }
 
-static Fact_def_use*
-CopyPropagation_newBoundaryFact (CopyPropagation *t, IR_function *func) {
+static Fact_def_use *
+CopyPropagation_newBoundaryFact(CopyPropagation *t, IR_function *func)
+{
     // TODO: return NEW(Fact_def_use, is_top?);
-    TODO();
+    // TODO();
+    return NEW(Fact_def_use, false);
 }
 
-static Fact_def_use*
-CopyPropagation_newInitialFact (CopyPropagation *t) {
+static Fact_def_use *
+CopyPropagation_newInitialFact(CopyPropagation *t)
+{
     // TODO: return NEW(Fact_def_use, is_top?);
-    TODO();
+    // TODO();
+    return NEW(Fact_def_use, true);
 }
 
 static void
-CopyPropagation_setInFact (CopyPropagation *t,
-                                        IR_block *blk,
-                                        Fact_def_use *fact) {
+CopyPropagation_setInFact(CopyPropagation *t,
+                          IR_block *blk,
+                          Fact_def_use *fact)
+{
     VCALL(t->mapInFact, set, blk, fact);
 }
 
 static void
-CopyPropagation_setOutFact (CopyPropagation *t,
-                                         IR_block *blk,
-                                         Fact_def_use *fact) {
+CopyPropagation_setOutFact(CopyPropagation *t,
+                           IR_block *blk,
+                           Fact_def_use *fact)
+{
     VCALL(t->mapOutFact, set, blk, fact);
 }
 
-static Fact_def_use*
-CopyPropagation_getInFact (CopyPropagation *t, IR_block *blk) {
+static Fact_def_use *
+CopyPropagation_getInFact(CopyPropagation *t, IR_block *blk)
+{
     return VCALL(t->mapInFact, get, blk);
 }
 
-static Fact_def_use*
-CopyPropagation_getOutFact (CopyPropagation *t, IR_block *blk) {
+static Fact_def_use *
+CopyPropagation_getOutFact(CopyPropagation *t, IR_block *blk)
+{
     return VCALL(t->mapOutFact, get, blk);
 }
 
 static bool
-CopyPropagation_meetInto (CopyPropagation *t,
-                          Fact_def_use *fact,
-                          Fact_def_use *target) {
-    if(fact->is_top) return false;
-    if(target->is_top) {
+CopyPropagation_meetInto(CopyPropagation *t,
+                         Fact_def_use *fact,
+                         Fact_def_use *target)
+{
+    if (fact->is_top)
+        return false;
+    if (target->is_top)
+    {
         target->is_top = false;
-        for_map(IR_var, IR_var, it, fact->def_to_use) {
+        for_map(IR_var, IR_var, it, fact->def_to_use)
+        {
             VCALL(target->def_to_use, insert, it->key, it->val);
             VCALL(target->use_to_def, insert, it->val, it->key);
         }
@@ -86,12 +102,13 @@ CopyPropagation_meetInto (CopyPropagation *t,
     bool updated = false;
     Map_IR_var_IR_var not_exist;
     Map_IR_var_IR_var_init(&not_exist);
-    for_map(IR_var, IR_var, it, target->def_to_use)
-        if(!VCALL(fact->def_to_use, exist, it->key) || VCALL(fact->def_to_use, get, it->key) != it->val) {
-            VCALL(not_exist, insert, it->key, it->val);
-            updated = true;
-        }
-    for_map(IR_var, IR_var, it, not_exist) {
+    for_map(IR_var, IR_var, it, target->def_to_use) if (!VCALL(fact->def_to_use, exist, it->key) || VCALL(fact->def_to_use, get, it->key) != it->val)
+    {
+        VCALL(not_exist, insert, it->key, it->val);
+        updated = true;
+    }
+    for_map(IR_var, IR_var, it, not_exist)
+    {
         VCALL(target->def_to_use, delete, it->key);
         VCALL(target->use_to_def, delete, it->val);
     }
@@ -99,50 +116,62 @@ CopyPropagation_meetInto (CopyPropagation *t,
     return updated;
 }
 
-void CopyPropagation_transferStmt (CopyPropagation *t,
-                                   IR_stmt *stmt,
-                                   Fact_def_use *fact) {
+void CopyPropagation_transferStmt(CopyPropagation *t,
+                                  IR_stmt *stmt,
+                                  Fact_def_use *fact)
+{
     IR_var new_def = VCALL(*stmt, get_def);
     //// copy_kill
-    if(new_def != IR_VAR_NONE) {
-        if(VCALL(fact->def_to_use, exist, new_def)) {
+    if (new_def != IR_VAR_NONE)
+    {
+        if (VCALL(fact->def_to_use, exist, new_def))
+        {
             IR_var use = VCALL(fact->def_to_use, get, new_def);
             /* TODO:
              * use is killed by new_def
              * VCALL(fact->def_to_use/use_to_def?, delete, use/new_def?);
-             */ 
-            TODO();
+             */
+            // TODO();
+            VCALL(fact->use_to_def, delete, use);
         }
-        if(VCALL(fact->use_to_def, exist, new_def)) {
+        if (VCALL(fact->use_to_def, exist, new_def))
+        {
             IR_var def = VCALL(fact->use_to_def, get, new_def);
             /* TODO:
              * def is killed by new_def
              * VCALL(fact->def_to_use/use_to_def?, delete, def/new_def?);
-             */ 
-            TODO();
+             */
+            // TODO();
+            VCALL(fact->def_to_use, delete, def);
         }
     }
     //// copy_gen
-    if(stmt->stmt_type == IR_ASSIGN_STMT) {
-        IR_assign_stmt *assign_stmt = (IR_assign_stmt*)stmt;
-        if(!assign_stmt->rs.is_const) {
+    if (stmt->stmt_type == IR_ASSIGN_STMT)
+    {
+        IR_assign_stmt *assign_stmt = (IR_assign_stmt *)stmt;
+        if (!assign_stmt->rs.is_const)
+        {
             IR_var def = assign_stmt->rd, use = assign_stmt->rs.var;
             /* TODO:
              * def is killed by new_def
              * VCALL(fact->def_to_use/use_to_def?, set, def/use?);
-             */ 
-            TODO();
+             */
+            // TODO();
+            VCALL(fact->def_to_use, set, def, use);
+            VCALL(fact->use_to_def, set, use, def);
         }
     }
 }
 
-bool CopyPropagation_transferBlock (CopyPropagation *t,
-                                                 IR_block *block,
-                                                 Fact_def_use *in_fact,
-                                                 Fact_def_use *out_fact) {
+bool CopyPropagation_transferBlock(CopyPropagation *t,
+                                   IR_block *block,
+                                   Fact_def_use *in_fact,
+                                   Fact_def_use *out_fact)
+{
     Fact_def_use *new_out_fact = CopyPropagation_newInitialFact(t);
     CopyPropagation_meetInto(t, in_fact, new_out_fact);
-    for_list(IR_stmt_ptr, i, block->stmts) {
+    for_list(IR_stmt_ptr, i, block->stmts)
+    {
         IR_stmt *stmt = i->val;
         CopyPropagation_transferStmt(t, stmt, new_out_fact);
     }
@@ -151,17 +180,19 @@ bool CopyPropagation_transferBlock (CopyPropagation *t,
     return updated;
 }
 
-void CopyPropagation_print_result (CopyPropagation *t, IR_function *func) {
+void CopyPropagation_print_result(CopyPropagation *t, IR_function *func)
+{
     printf("Function %s: Copy Propagation Result\n", func->func_name);
-    for_list(IR_block_ptr, i, func->blocks) {
+    for_list(IR_block_ptr, i, func->blocks)
+    {
         IR_block *blk = i->val;
         printf("=================\n");
-        printf("{Block%s %p}\n", blk == func->entry ? "(Entry)" :
-                                 blk == func->exit ? "(Exit)" : "",
+        printf("{Block%s %p}\n", blk == func->entry ? "(Entry)" : blk == func->exit ? "(Exit)"
+                                                                                    : "",
                blk);
         IR_block_print(blk, stdout);
         Fact_def_use *in_fact = VCALL(*t, getInFact, blk),
-                *out_fact = VCALL(*t, getOutFact, blk);
+                     *out_fact = VCALL(*t, getOutFact, blk);
         printf("[In(top:%d)]:  ", in_fact->is_top);
         for_map(IR_var, IR_var, j, in_fact->def_to_use)
             printf("{v%u := v%u} ", j->key, j->val);
@@ -174,20 +205,20 @@ void CopyPropagation_print_result (CopyPropagation *t, IR_function *func) {
     }
 }
 
-void CopyPropagation_init(CopyPropagation *t) {
+void CopyPropagation_init(CopyPropagation *t)
+{
     const static struct CopyPropagation_virtualTable vTable = {
-            .teardown        = CopyPropagation_teardown,
-            .isForward       = CopyPropagation_isForward,
-            .newBoundaryFact = CopyPropagation_newBoundaryFact,
-            .newInitialFact  = CopyPropagation_newInitialFact,
-            .setInFact       = CopyPropagation_setInFact,
-            .setOutFact      = CopyPropagation_setOutFact,
-            .getInFact       = CopyPropagation_getInFact,
-            .getOutFact      = CopyPropagation_getOutFact,
-            .meetInto        = CopyPropagation_meetInto,
-            .transferBlock   = CopyPropagation_transferBlock,
-            .printResult     = CopyPropagation_print_result
-    };
+        .teardown = CopyPropagation_teardown,
+        .isForward = CopyPropagation_isForward,
+        .newBoundaryFact = CopyPropagation_newBoundaryFact,
+        .newInitialFact = CopyPropagation_newInitialFact,
+        .setInFact = CopyPropagation_setInFact,
+        .setOutFact = CopyPropagation_setOutFact,
+        .getInFact = CopyPropagation_getInFact,
+        .getOutFact = CopyPropagation_getOutFact,
+        .meetInto = CopyPropagation_meetInto,
+        .transferBlock = CopyPropagation_transferBlock,
+        .printResult = CopyPropagation_print_result};
     t->vTable = &vTable;
     Map_IR_block_ptr_Fact_def_use_ptr_init(&t->mapInFact);
     Map_IR_block_ptr_Fact_def_use_ptr_init(&t->mapOutFact);
@@ -196,17 +227,20 @@ void CopyPropagation_init(CopyPropagation *t) {
 //// ============================ Optimize ============================
 
 // 将所有use变为copy的def变量
-static void block_replace_available_use_copy (CopyPropagation *t, IR_block *blk) {
+static void block_replace_available_use_copy(CopyPropagation *t, IR_block *blk)
+{
     Fact_def_use *blk_in_fact = VCALL(*t, getInFact, blk);
     Fact_def_use *new_in_fact = CopyPropagation_newInitialFact(t);
     CopyPropagation_meetInto(t, blk_in_fact, new_in_fact);
-    for_list(IR_stmt_ptr, i, blk->stmts) {
+    for_list(IR_stmt_ptr, i, blk->stmts)
+    {
         IR_stmt *stmt = i->val;
         IR_use use = VCALL(*stmt, get_use_vec);
-        for(int j = 0; j < use.use_cnt; j++)
-            if(!use.use_vec[j].is_const) {
+        for (int j = 0; j < use.use_cnt; j++)
+            if (!use.use_vec[j].is_const)
+            {
                 IR_var use_var = use.use_vec[j].var;
-                if(VCALL(new_in_fact->def_to_use, exist, use_var))
+                if (VCALL(new_in_fact->def_to_use, exist, use_var))
                     use.use_vec[j].var = VCALL(new_in_fact->def_to_use, get, use_var);
             }
         CopyPropagation_transferStmt(t, stmt, new_in_fact);
@@ -214,13 +248,11 @@ static void block_replace_available_use_copy (CopyPropagation *t, IR_block *blk)
     RDELETE(Fact_def_use, new_in_fact);
 }
 
-void CopyPropagation_replace_available_use_copy (CopyPropagation *t, IR_function *func) {
-    for_list(IR_block_ptr, j, func->blocks) {
+void CopyPropagation_replace_available_use_copy(CopyPropagation *t, IR_function *func)
+{
+    for_list(IR_block_ptr, j, func->blocks)
+    {
         IR_block *blk = j->val;
         block_replace_available_use_copy(t, blk);
     }
 }
-
-
-
-
--- src/IR_optimize/live_variable_analysis.c
+++ src/IR_optimize/live_variable_analysis.c
@@ -6,7 +6,8 @@
 
 //// ============================ Dataflow Analysis ============================
 
-static void LiveVariableAnalysis_teardown(LiveVariableAnalysis *t) {
+static void LiveVariableAnalysis_teardown(LiveVariableAnalysis *t)
+{
     for_map(IR_block_ptr, Set_ptr_IR_var, i, t->mapInFact)
         DELETE(i->val);
     for_map(IR_block_ptr, Set_ptr_IR_var, i, t->mapOutFact)
@@ -16,59 +17,70 @@ static void LiveVariableAnalysis_teardown(LiveVariableAnalysis *t) {
 }
 
 static bool
-LiveVariableAnalysis_isForward (LiveVariableAnalysis *t) {
+LiveVariableAnalysis_isForward(LiveVariableAnalysis *t)
+{
     // TODO: return isForward?;
-    TODO();
+    // TODO();
+    return false;
 }
 
-static Set_IR_var*
-LiveVariableAnalysis_newBoundaryFact (LiveVariableAnalysis *t, IR_function *func) {
+static Set_IR_var *
+LiveVariableAnalysis_newBoundaryFact(LiveVariableAnalysis *t, IR_function *func)
+{
     return NEW(Set_IR_var);
 }
 
-static Set_IR_var*
-LiveVariableAnalysis_newInitialFact (LiveVariableAnalysis *t) {
+static Set_IR_var *
+LiveVariableAnalysis_newInitialFact(LiveVariableAnalysis *t)
+{
     return NEW(Set_IR_var); // May Analysis => Bottom
 }
 
 static void
-LiveVariableAnalysis_setInFact (LiveVariableAnalysis *t,
-                                IR_block *blk,
-                                Set_IR_var *fact) {
+LiveVariableAnalysis_setInFact(LiveVariableAnalysis *t,
+                               IR_block *blk,
+                               Set_IR_var *fact)
+{
     VCALL(t->mapInFact, set, blk, fact);
 }
 
 static void
-LiveVariableAnalysis_setOutFact (LiveVariableAnalysis *t,
-                                 IR_block *blk,
-                                 Set_IR_var *fact) {
+LiveVariableAnalysis_setOutFact(LiveVariableAnalysis *t,
+                                IR_block *blk,
+                                Set_IR_var *fact)
+{
     VCALL(t->mapOutFact, set, blk, fact);
 }
 
-static Set_IR_var*
-LiveVariableAnalysis_getInFact (LiveVariableAnalysis *t, IR_block *blk) {
+static Set_IR_var *
+LiveVariableAnalysis_getInFact(LiveVariableAnalysis *t, IR_block *blk)
+{
     return VCALL(t->mapInFact, get, blk);
 }
 
-static Set_IR_var*
-LiveVariableAnalysis_getOutFact (LiveVariableAnalysis *t, IR_block *blk) {
+static Set_IR_var *
+LiveVariableAnalysis_getOutFact(LiveVariableAnalysis *t, IR_block *blk)
+{
     return VCALL(t->mapOutFact, get, blk);
 }
 
 static bool
-LiveVariableAnalysis_meetInto (LiveVariableAnalysis *t,
-                               Set_IR_var *fact,
-                               Set_IR_var *target) {
+LiveVariableAnalysis_meetInto(LiveVariableAnalysis *t,
+                              Set_IR_var *fact,
+                              Set_IR_var *target)
+{
     /* TODO:
      * meet: union/intersect?
      * return VCALL(*target, union_with/intersect_with?, fact);
      */
-    TODO();
+    // TODO();
+    return VCALL(*target, union_with, fact);
 }
 
-void LiveVariableAnalysis_transferStmt (LiveVariableAnalysis *t,
-                                        IR_stmt *stmt,
-                                        Set_IR_var *fact) {
+void LiveVariableAnalysis_transferStmt(LiveVariableAnalysis *t,
+                                       IR_stmt *stmt,
+                                       Set_IR_var *fact)
+{
     IR_var def = VCALL(*stmt, get_def);
     IR_use use = VCALL(*stmt, get_use_vec);
     /* TODO:
@@ -80,23 +92,38 @@ void LiveVariableAnalysis_transferStmt (LiveVariableAnalysis *t,
      *      if(!use_val.is_const) {
      *          IR_var use = use_val.var;
      *          VCALL(*fact, insert/delete?, use); // kill/gen ?
-     *      }     
+     *      }
      *  }
      * def:
      *  if(def != IR_VAR_NONE) { // 生成新def
      *      VCALL(*fact, insert/delete?, def); // kill/gen ?
      *  }
      */
-    TODO();
+    // TODO();
+    if (def != IR_VAR_NONE)
+    {
+        VCALL(*fact, delete, def);
+    }
+    for (unsigned i = 0; i < use.use_cnt; i++)
+    {
+        IR_val use_val = use.use_vec[i];
+        if (!use_val.is_const)
+        {
+            IR_var use = use_val.var;
+            VCALL(*fact, insert, use);
+        }
+    }
 }
 
-bool LiveVariableAnalysis_transferBlock (LiveVariableAnalysis *t,
-                                         IR_block *block,
-                                         Set_IR_var *in_fact,
-                                         Set_IR_var *out_fact) {
+bool LiveVariableAnalysis_transferBlock(LiveVariableAnalysis *t,
+                                        IR_block *block,
+                                        Set_IR_var *in_fact,
+                                        Set_IR_var *out_fact)
+{
     Set_IR_var *new_in_fact = LiveVariableAnalysis_newInitialFact(t);
     LiveVariableAnalysis_meetInto(t, out_fact, new_in_fact);
-    rfor_list(IR_stmt_ptr, i, block->stmts) { // 倒序遍历
+    rfor_list(IR_stmt_ptr, i, block->stmts)
+    { // 倒序遍历
         IR_stmt *stmt = i->val;
         LiveVariableAnalysis_transferStmt(t, stmt, new_in_fact);
     }
@@ -105,17 +132,19 @@ bool LiveVariableAnalysis_transferBlock (LiveVariableAnalysis *t,
     return updated;
 }
 
-void LiveVariableAnalysis_print_result(LiveVariableAnalysis *t, IR_function *func) {
+void LiveVariableAnalysis_print_result(LiveVariableAnalysis *t, IR_function *func)
+{
     printf("Function %s: Live Variable Analysis Result\n", func->func_name);
-    for_list(IR_block_ptr, i, func->blocks) {
+    for_list(IR_block_ptr, i, func->blocks)
+    {
         IR_block *blk = i->val;
         printf("=================\n");
-        printf("{Block%s %p}\n", blk == func->entry ? "(Entry)" :
-                                      blk == func->exit ? "(Exit)" : "",
-                                      blk);
+        printf("{Block%s %p}\n", blk == func->entry ? "(Entry)" : blk == func->exit ? "(Exit)"
+                                                                                    : "",
+               blk);
         IR_block_print(blk, stdout);
         Set_IR_var *in_fact = VCALL(*t, getInFact, blk),
-                *out_fact = VCALL(*t, getOutFact, blk);
+                   *out_fact = VCALL(*t, getOutFact, blk);
         printf("[In]:  ");
         for_set(IR_var, var, *in_fact)
             printf("v%u ", var->key);
@@ -128,20 +157,20 @@ void LiveVariableAnalysis_print_result(LiveVariableAnalysis *t, IR_function *fun
     }
 }
 
-void LiveVariableAnalysis_init(LiveVariableAnalysis *t) {
+void LiveVariableAnalysis_init(LiveVariableAnalysis *t)
+{
     const static struct LiveVariableAnalysis_virtualTable vTable = {
-            .teardown        = LiveVariableAnalysis_teardown,
-            .isForward       = LiveVariableAnalysis_isForward,
-            .newBoundaryFact = LiveVariableAnalysis_newBoundaryFact,
-            .newInitialFact  = LiveVariableAnalysis_newInitialFact,
-            .setInFact       = LiveVariableAnalysis_setInFact,
-            .setOutFact      = LiveVariableAnalysis_setOutFact,
-            .getInFact       = LiveVariableAnalysis_getInFact,
-            .getOutFact      = LiveVariableAnalysis_getOutFact,
-            .meetInto        = LiveVariableAnalysis_meetInto,
-            .transferBlock   = LiveVariableAnalysis_transferBlock,
-            .printResult     = LiveVariableAnalysis_print_result
-    };
+        .teardown = LiveVariableAnalysis_teardown,
+        .isForward = LiveVariableAnalysis_isForward,
+        .newBoundaryFact = LiveVariableAnalysis_newBoundaryFact,
+        .newInitialFact = LiveVariableAnalysis_newInitialFact,
+        .setInFact = LiveVariableAnalysis_setInFact,
+        .setOutFact = LiveVariableAnalysis_setOutFact,
+        .getInFact = LiveVariableAnalysis_getInFact,
+        .getOutFact = LiveVariableAnalysis_getOutFact,
+        .meetInto = LiveVariableAnalysis_meetInto,
+        .transferBlock = LiveVariableAnalysis_transferBlock,
+        .printResult = LiveVariableAnalysis_print_result};
     t->vTable = &vTable;
     Map_IR_block_ptr_Set_ptr_IR_var_init(&t->mapInFact);
     Map_IR_block_ptr_Set_ptr_IR_var_init(&t->mapOutFact);
@@ -149,16 +178,20 @@ void LiveVariableAnalysis_init(LiveVariableAnalysis *t) {
 
 //// ============================ Optimize ============================
 
-static bool block_remove_dead_def (LiveVariableAnalysis *t, IR_block *blk) {
+static bool block_remove_dead_def(LiveVariableAnalysis *t, IR_block *blk)
+{
     bool updated = false;
     Set_IR_var *blk_out_fact = VCALL(*t, getOutFact, blk);
     Set_IR_var *new_out_fact = LiveVariableAnalysis_newInitialFact(t);
     LiveVariableAnalysis_meetInto(t, blk_out_fact, new_out_fact);
-    rfor_list(IR_stmt_ptr, i, blk->stmts) {
+    rfor_list(IR_stmt_ptr, i, blk->stmts)
+    {
         IR_stmt *stmt = i->val;
-        if(stmt->stmt_type == IR_OP_STMT || stmt->stmt_type == IR_ASSIGN_STMT) {
+        if (stmt->stmt_type == IR_OP_STMT || stmt->stmt_type == IR_ASSIGN_STMT)
+        {
             IR_var def = VCALL(*stmt, get_def);
-            if(def == IR_VAR_NONE) continue;
+            if (def == IR_VAR_NONE)
+                continue;
             /* TODO:
              * def具有什么性质可以被标记为死代码?
              * if(VCALL(*new_out_fact, exist, def) == true/false?) {
@@ -166,7 +199,12 @@ static bool block_remove_dead_def (LiveVariableAnalysis *t, IR_block *blk) {
              *      updated = true;
              *  }
              */
-            TODO();
+            // TODO();
+            if (VCALL(*new_out_fact, exist, def) == false)
+            {
+                stmt->dead = true;
+                updated = true;
+            }
         }
         LiveVariableAnalysis_transferStmt(t, stmt, new_out_fact);
     }
@@ -175,9 +213,11 @@ static bool block_remove_dead_def (LiveVariableAnalysis *t, IR_block *blk) {
     return updated;
 }
 
-bool LiveVariableAnalysis_remove_dead_def (LiveVariableAnalysis *t, IR_function *func) {
+bool LiveVariableAnalysis_remove_dead_def(LiveVariableAnalysis *t, IR_function *func)
+{
     bool updated = false;
-    for_list(IR_block_ptr, j, func->blocks) {
+    for_list(IR_block_ptr, j, func->blocks)
+    {
         IR_block *blk = j->val;
         updated |= block_remove_dead_def(t, blk);
     }
